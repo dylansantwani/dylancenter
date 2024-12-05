40d2f7b2c3a4b019f09f12762855c3608b229b74
dylansantwani <144082336+dylansantwani@users.noreply.github.com>
2024-12-04 20:35:35 -0600

    add site!!!!

---
+++ check.js
@@ -0,0 +1,36 @@
+domains = [
+    'dylansantwani.com',
+    'dylansantwani.github.io',
+    'dsgame2.github.io',
+    'watch.dylansantwani.com',
+    'tvplayer.pages.dev',
+    'cloudflarepage-1j1.pages.dev',
+    'newgame-d29.pages.dev'
+]
+active = [
+    'T',
+    'T',
+    'T',
+    'T',
+    'T',
+    'T',
+    'T'
+]
+for (let i = 0; i < domains.length; i++) {
+    let divtoadd = document.getElementById('checkfalse');
+    let checkelement = document.createElement('p');
+    checkelement.innerText = domains[i];
+    if (active[i] == "T") {
+        let classes = "text-green-400 mr-[25px] flex cursor-pointer";
+        checkelement.onclick = function () {
+            window.location.href = 'https://' + domains[i]
+        }; checkelement.className += classes;
+        divtoadd.appendChild(checkelement)
+    }
+    else {
+        let classes = "text-rose-700 mr-[25px] flex cursor-pointer";
+        checkelement.className += classes;
+        divtoadd.appendChild(checkelement)
+    }
+
+}
